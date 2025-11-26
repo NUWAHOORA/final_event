85c7077fa16144fed35ee01310c09207bf809878
NUWAHOORA <nuwahoorabrian02@gmail.com>
2025-11-26 20:22:28 +0300

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/final_event">
         <Routes>
           <Route path="/" element={<Auth />} />
           <Route path="/auth" element={<Auth />} />
